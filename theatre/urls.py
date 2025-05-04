832e6085cec672de3b497c75283af7fb5dafa21b
anton <anton@gmail.com>
2025-05-04 14:44:12 +0200

    Обновил проект: добавлены ViewSet'ы и API маршруты

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 from .views import PerformanceDetailView, BuyTicketView, TicketConfirmationView, GenreViewSet, ActorViewSet, PlayViewSet, TheatreHallViewSet, PerformanceViewSet, ReservationViewSet, TicketViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -40,3 +43,16 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Theatre API",
+        default_version='v1',
+        description="Документация для театрального API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+# в urlpatterns:
+path('api/doc/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
